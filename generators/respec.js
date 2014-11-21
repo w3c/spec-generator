caed5fbb7342681039f43cef8ae3a6a9301b2f50
Robin Berjon <robin@berjon.com>
2014-11-21 15:13:14 +0100

    handle timeouts ourselves because Phantom doesn't do it

--- generators/respec.js
+++ generators/respec.js
@@ -2,15 +2,27 @@
 var phantom = require("phantomjs")
 ,   execFile = require("child_process").execFile
 ,   jn = require("path").join
-,   r2hPath = jn(__dirname, "../../node_modules/respec/tools/respec2html.js")
+,   r2hPath = jn(__dirname, "../node_modules/respec/tools/respec2html.js")
 ;
 
 exports.generate = function (url, cb) {
+    console.log("Generating", url);
+    // Phantom's own timeouts are never reaching us for some reason, so we do our own
+    var timedout = false;
+    setTimeout(
+        function () {
+            timedout = true;
+            cb({ status: 500, message: "Processing timed out." });
+        }
+    ,   10000
+    );
     execFile(
         phantom.path
     ,   ["--ssl-protocol=any", r2hPath, url]
     ,   function (err, stdout, stderr) {
-            if (err) return cb({ status: 500, message: err + "\n" + stderr });
+            if (timedout) return;
+            // note: Phantom prints error on stdout even when you use console.error()
+            if (err) return cb({ status: 500, message: err + "\n" + (stderr || stdout || "") });
             cb(null, stdout);
         }
     );
