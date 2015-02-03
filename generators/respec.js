c6f13065c8c187c95dce4a61009b3abef7e55f21
Dominique Hazael-Massieux <dom@w3.org>
2015-02-03 18:29:43 +0100

    don't always timeout; use stderr only close #2

--- generators/respec.js
+++ generators/respec.js
@@ -13,7 +13,7 @@ exports.generate = function (url, params, cb) {
     console.log("Generating", url);
     // Phantom's own timeouts are never reaching us for some reason, so we do our own
     var timedout = false;
-    setTimeout(
+    var tickingBomb = setTimeout(
         function () {
             timedout = true;
             cb({ status: 500, message: "Processing timed out." });
@@ -25,8 +25,8 @@ exports.generate = function (url, params, cb) {
     ,   ["--ssl-protocol=any", r2hPath, url]
     ,   function (err, stdout, stderr) {
             if (timedout) return;
-            // note: Phantom prints error on stdout even when you use console.error()
-            if (err) return cb({ status: 500, message: err + "\n" + (stderr || stdout || "") });
+            clearTimeout(tickingBomb);
+            if (err) return cb({ status: 500, message: err + "\n" + (stderr || "") });
             cb(null, stdout);
         }
     );
