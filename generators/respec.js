ec0d673851b7d399dd56098976802a4061ea66ab
plehegar <plh@w3.org>
2015-12-08 10:31:15 -0500

    Fixed comment to reflect the new default

--- generators/respec.js
+++ generators/respec.js
@@ -27,7 +27,7 @@ exports.generate = function (url, params, cb) {
     execFile(
         phantom.path
     ,   ["--ssl-protocol=any", r2hPath, url]
-    ,   { maxBuffer: 1600*1024 } // default * 2
+    ,   { maxBuffer: 1600*1024 } // default * 8
     ,   function (err, stdout, stderr) {
             if (timedout) return;
             clearTimeout(tickingBomb);
