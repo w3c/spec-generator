0fa9a072feaa31e816aa76418ab3fb12e52ad3f8
Marcos Cáceres <mcaceres@mozilla.com>
2018-11-15 14:41:13 +1100

    fix(generators/respec): use & not ;

--- generators/respec.js
+++ generators/respec.js
@@ -1,21 +1,16 @@
+const { fetchAndWrite : respecWriter } = require("respec/tools/respecDocWriter");
 
-var u = require("url")
-,   querystring = require("querystring")
-,   respecWriter = require("respec/tools/respecDocWriter").fetchAndWrite
-;
-
-exports.generate = function (url, params, cb) {
-    url = u.parse(url);
-    // respec use ";" as query string separators
-    var qs = querystring.parse(url.query, ";")
-    for (var k in params) if (params.hasOwnProperty(k)) qs[k] = params[k];
-    url.search = querystring.stringify(qs, ";");
-    url = u.format(url);
+exports.generate = async function generate(urlToGenerate, params, cb) {
+  const url = new URL(urlToGenerate);
+  for (const [key, value] of Object.entries(params)) {
+    url.searchParams.set(key, value);
+  }
+  const opts = { timeout: 20000, disableSandbox: true };
+  try {
     console.log("Generating", url);
-
-    respecWriter(url, '/dev/null', {}, {timeout: 20000, disableSandbox: true}).then(function(html) {
-        cb(null, html);
-    }).catch(function (err) {
-        cb({ status: 500, message: err.message });
-    });
-};
+    const html = await respecWriter(url.href, "/dev/null", {}, opts);
+    cb(null, html);
+  } catch (err) {
+    cb({ status: 500, message: err.message });
+  }
+}
