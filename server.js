e163b0a4b9e10502192370a267f67579d495daf3
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-10-21 02:52:44 +0530

    extract getPreviousVersionInfo function

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(
 // Listens to GET at the root, expects two required query string parameters:
 //  type:   the type of the generator (case-insensitive)
 //  url:    the URL to the source document
-app.get("/", function (req, res) {
+app.get("/", async function (req, res) {
     var type =
         typeof req.query.type === "string"
             ? req.query.type.toLowerCase()
@@ -59,43 +59,23 @@ app.get("/", function (req, res) {
     }
     specURL.searchParams.set("publishDate", publishDate);
 
-    // if shortName was provided, we collect info on previous version
     if (shortName) {
-        request.get("https://www.w3.org/TR/" + shortName + "/", function(error, response, body) {
-            if (error)
-                return res.status(400).json({error: error});
-            else if (response && response.statusCode >= 400 && response.statusCode < 500)
-                return res.status(response.statusCode).json({error: response.statusMessage});
-            var document = (new JSDOM(body)).window.document
-            ,   dl = document.querySelector('body div.head dl')
-            ,   thisURI
-            ,   previousURI;
-            if (dl) {
-                dl.querySelectorAll('dt').forEach(dt => {
-                    var txt = dt.textContent
-                        .toLocaleLowerCase()
-                        .replace(':', '')
-                        .replace('published', '')
-                        .trim();
-                    var dd = dt.nextElementSibling;
-                    if (txt === "this version")
-                        thisURI = dd.querySelector('a').href;
-                    else if (/^previous version(?:s)?$/.test(txt))
-                        previousURI = dd.querySelector('a').href;
-                });
-            }
-            if (!thisURI) return res.status(500).json({ error: "Couldn't find a 'This version' uri in the previous version." });
-            var thisDate = thisURI.match(/[1-2][0-9]{7}/)[0]
-            ,   prev     = (thisDate === publishDate.replace(/\-/g, '')) ? previousURI : thisURI
-            ,   pDate    = prev.match(/[1-2][0-9]{7}/)[0];
-            specURL.searchParams.set("previousMaturity", prev.match(/\/TR\/[0-9]{4}\/([A-Z]+)/)[1]);
-            specURL.searchParams.set("previousPublishDate", pDate.substring(0, 4) + '-' +
-            pDate.substring(4, 6) + '-' + pDate.substring(6, 8));
-            generate(specURL.href);
-        });
-    } else {
-        generate(specURL.href);
+        try {
+            const {
+                previousMaturity,
+                previousPublishDate,
+            } = await getPreviousVersionInfo(shortName, publishDate);
+            specURL.searchParams.set("previousMaturity", previousMaturity);
+            specURL.searchParams.set(
+                "previousPublishDate",
+                previousPublishDate,
+            );
+        } catch ({ statusCode, error }) {
+            return res.status(statusCode).json({ error });
+        }
     }
+
+    generate(specURL.href);
 
     async function generate(url) {
         // if there's an error we get an err object with status and message, otherwise we get content
@@ -201,12 +181,80 @@ app.post("/", (req, res) => {
 });
 
 /**
-* Start listening for HTTP requests.
-*
-* @param {Number} port - port number to use (optional); defaults to environment variable `$PORT` if exists, and to `80` if not
-* @returns {Object} a `http.Server`; cf https://nodejs.org/dist/latest-v8.x/docs/api/http.html#http_class_http_server
-*/
+ * @param {string} shortName
+ * @param {string} publishDate
+ * @returns {Promise<{ previousMaturity: string, previousPublishDate: string }>}
+ * @throws {Promise<{ statusCode: number, error: string }>}
+ */
+function getPreviousVersionInfo(shortName, publishDate) {
+    return new Promise((resolve, reject) => {
+        const url = "https://www.w3.org/TR/" + shortName + "/";
+        request.get(url, (error, response, body) => {
+            if (error) {
+                return reject({ statusCode: 400, error });
+            }
 
+            if (
+                response &&
+                response.statusCode >= 400 &&
+                response.statusCode < 500
+            ) {
+                const { statusCode, statusMessage } = response;
+                return reject({ statusCode, error: statusMessage });
+            }
+
+            const document = new JSDOM(body).window.document;
+            const dl = document.querySelector("body div.head dl");
+
+            let thisURI;
+            let previousURI;
+            if (dl) {
+                for (const dt of dl.querySelectorAll("dt")) {
+                    const txt = dt.textContent
+                        .toLocaleLowerCase()
+                        .replace(":", "")
+                        .replace("published", "")
+                        .trim();
+                    const dd = dt.nextElementSibling;
+                    if (txt === "this version") {
+                        thisURI = dd.querySelector("a").href;
+                    } else if (/^previous version(?:s)?$/.test(txt)) {
+                        previousURI = dd.querySelector("a").href;
+                    }
+                }
+            }
+            if (!thisURI) {
+                return reject({
+                    statusCode: 5000,
+                    error: `Couldn't find a 'This version' uri in the previous version.`,
+                });
+            }
+
+            const thisDate = thisURI.match(/[1-2][0-9]{7}/)[0];
+            const prev =
+                thisDate === publishDate.replace(/\-/g, "")
+                    ? previousURI
+                    : thisURI;
+            const pDate = prev.match(/[1-2][0-9]{7}/)[0];
+
+            const previousMaturity = prev.match(/\/TR\/[0-9]{4}\/([A-Z]+)/)[1];
+            const previousPublishDate =
+                pDate.substring(0, 4) +
+                "-" +
+                pDate.substring(4, 6) +
+                "-" +
+                pDate.substring(6, 8);
+            resolve({ previousMaturity, previousPublishDate });
+        });
+    });
+}
+
+/**
+ * Start listening for HTTP requests.
+ *
+ * @param {Number} port - port number to use (optional); defaults to environment variable `$PORT` if exists, and to `80` if not
+ * @returns {Object} a `http.Server`; cf https://nodejs.org/dist/latest-v8.x/docs/api/http.html#http_class_http_server
+ */
 app.start = (port) => {
     if (port)
         return app.listen(port);
