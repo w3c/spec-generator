dc753079d7fd9c1842d73ddcc536fef2e1b99aa0
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2025-12-11 12:11:40 -0500

    Report all messages on failure or when output=messages is specified
    This also fixes a bug in the bikeshed handler, which caused processing to fail
    on fatal errors even if die-on=nothing was specified.

--- generators/respec.d.ts
+++ generators/respec.d.ts
@@ -9,13 +9,24 @@ interface ToHTMLOptions {
   onWarning?: (warning: any) => void;
 }
 
+interface ToHTMLMessage {
+  details?: string;
+  elements?: any[];
+  hint?: string;
+  message: string;
+  name: `ReSpec${"Error" | "Warning"}`;
+  plugin: string;
+  stack?: string;
+  title?: string;
+}
+
 declare module "respec" {
   export function toHTML(
     url: string,
     options?: ToHTMLOptions,
   ): Promise<{
     html: string;
-    errors: any[];
-    warnings: any[];
+    errors: ToHTMLMessage[];
+    warnings: ToHTMLMessage[];
   }>;
 }
